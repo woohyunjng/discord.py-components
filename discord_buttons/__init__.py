e344999db27505c5648150a1d4a4133bac7355b7
kiki7000 <devkiki7000@gmail.com>
2021-04-23 16:20:02 +0900

    feat: upgrade version to 0.3.1

--- discord_buttons/__init__.py
+++ discord_buttons/__init__.py
@@ -2,7 +2,7 @@ from .client import DiscordButton
 from .button import Button
 
 __name__ = "discord_buttons"
-__version__ = "0.3"
+__version__ = "0.3.1"
 
 __author__ = "kiki7000"
 __license__ = "MIT"
