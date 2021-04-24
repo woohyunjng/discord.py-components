2450a66f2b1bfa07816725bf1aac19c8ebcd6fa3
kiki7000 <devkiki7000@gmail.com>
2021-04-24 19:49:46 +0900

    feat: buttom properties

--- discord_buttons/button.py
+++ discord_buttons/button.py
@@ -1,3 +1,5 @@
+from discord import InvalidArgument
+
 from typing import Union, Optional
 from uuid import uuid1
 from random import randint
@@ -30,15 +32,85 @@ class ButtonStyle:
 
 
 class Button:
-    __slots__ = ("style", "label", "id", "url")
+    __slots__ = ("_style", "_label", "_id", "_url")
 
     def __init__(
-        self, *, label: str, style: int = 1, id: Optional[str] = None, url: Optional[str] = None
+        self,
+        *,
+        label: str,
+        style: int = ButtonStyle.gray,
+        id: Optional[str] = None,
+        url: Optional[str] = None,
     ):
-        self.style = style
-        self.label = label
-        if url:
-            self.id = None
+        if style == ButtonStyle.URL and not url:
+            raise InvalidArgument("You must provide a URL when the style is URL")
+        if style == ButtonStyle.URL and id:
+            raise InvalidArgument("You musn't use both id and url")
+        if not (1 <= style <= ButtonStyle.URL):
+            raise InvalidArgument(f"style must be in between 1, {ButtonStyle.URL}")
+
+        if not len(label):
+            raise InvalidArgument("Label musn't be empty")
+
+        self._style = style
+        self._label = label
+        self._url = url
+
+        if not self.style == ButtonStyle.URL:
+            self._id = id or str(uuid1())
         else:
-            self.id = id or str(uuid1())
-        self.url = url
+            self._id = None
+
+    def to_dict(self) -> dict:
+        return {
+            "type": 2,
+            "style": self.style,
+            "label": self.label,
+            "custom_id": self.id,
+            "url": self.url if self.style == ButtonStyle.URL else None,
+        }
+
+    @property
+    def style(self) -> int:
+        return self._style
+
+    @property
+    def label(self) -> str:
+        return self._label
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @label.setter
+    def label(self, value: str):
+        if not len(value):
+            raise InvalidArgument("Label musn't be empty")
+
+        self._label = value
+
+    @url.setter
+    def url(self, value: str):
+        if self.style != ButtonStyle.URL:
+            raise InvalidArgument("button style is not URL")
+
+        self._url = value
+
+    @id.setter
+    def id(self, value: str):
+        if self.style == ButtonStyle.URL:
+            raise InvalidArgument("button style is URL")
+
+        self._id = id
+
+    def __repr__(self) -> str:
+        id_st = f"id='{self.id}'" if self.id else ""
+        url_st = f"url='{self.url}'" if self.url else ""
+        return f"<Button style={self.style} label='{self.label}' {url_st} {id_st}>"
+
+    def __str__(self) -> str:
+        return self.__repr__()
