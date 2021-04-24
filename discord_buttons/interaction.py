2851f770ad68cd4f2d6a4f898600fa1f91dbb5b7
kiki7000 <devkiki7000@gmail.com>
2021-04-24 20:23:32 +0900

    feat: InteractionType

---
+++ discord_buttons/interaction.py
@@ -0,0 +1,8 @@
+__all__ = ("InteractionType",)
+
+
+class InteractionType:
+    Pong = 1
+    ChannelMessageWithSource = 4
+    DeferredChannelMessageWithSource = 5
+    IgnoredChannelMessageWithSource = 6
