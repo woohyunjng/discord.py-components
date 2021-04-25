041832c6c6814b6bf649f86992e8b9673c2b05df
kiki7000 <devkiki7000@gmail.com>
2021-04-25 16:58:25 +0900

    feat: upgrade version to 0.3.5

--- discord_buttons/__init__.py
+++ discord_buttons/__init__.py
@@ -4,7 +4,7 @@ from .context import Context
 from .interaction import InteractionType, FlagsType
 
 __name__ = "discord_buttons"
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 __author__ = "kiki7000"
 __license__ = "MIT"
