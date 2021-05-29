fd15e498abeb4a97df771c9b44ada2cca4309872
kiki7000 <devkiki7000@gmail.com>
2021-05-29 22:51:49 +0900

    feat: why __init__.py disappear

---
+++ discord_components/__init__.py
@@ -0,0 +1,12 @@
+from .client import *
+from .button import *
+from .select import *
+from .context import *
+from .interaction import *
+from .component import *
+
+__name__ = "discord_components"
+__version__ = "0.5.2.3"
+
+__author__ = "kiki7000"
+__license__ = "MIT"
