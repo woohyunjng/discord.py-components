74b440c11530eaf8bdfb0086f626d0900791ccaf
PythonSerious <63519852+PythonSerious@users.noreply.github.com>
2021-05-29 13:26:39 -0400

    Update simple.py
    add wait_for with a check and asyncio

--- examples/simple.py
+++ examples/simple.py
@@ -1,5 +1,6 @@
 from discord.ext.commands import Bot
 from discord_components import DiscordComponents, Button, ButtonStyle, InteractionType
+import asyncio
 
 bot = Bot("!")
 
@@ -25,6 +26,32 @@ async def button(ctx):
             Button(style=ButtonStyle.URL, label="URL", url="https://www.example.com"),
         ],
     )
+
+
+@bot.command()
+async def waitforclick(ctx):
+    m = await ctx.send(
+        "Buttons waiting for a click",
+        components=[
+            Button(style=ButtonStyle.red, label="Click Me!"),
+        ])
+
+    def check(res):
+        return ctx.author == res.user and res.channel == ctx.channel
+
+    try:
+        res = await bot.wait_for("button_click", check=check, timeout=15)
+        await res.respond(
+            type=InteractionType.ChannelMessageWithSource, content=f"{res.component.label} pressed"
+        )
+
+    except asyncio.TimeoutError:
+        await m.edit(
+            "Prompt timed out!",
+            components=[
+                Button(style=ButtonStyle.red, label="Timed out!", disabled=True),
+            ]
+        )
 
 
 @bot.event
