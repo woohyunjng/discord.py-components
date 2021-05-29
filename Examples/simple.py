cc32a8bf0d5b445aa72087113e6377a0e040161e
PythonSerious <63519852+PythonSerious@users.noreply.github.com>
2021-05-29 03:47:05 -0400

    Update simple.py

--- Examples/simple.py
+++ Examples/simple.py
@@ -4,29 +4,45 @@ from discord_components import DiscordComponents, Button, ButtonStyle, Interacti
 
 bot = commands.Bot("!")
 
-ddb = DiscordComponents(bot)
+
+
 
 @bot.event
-async def on_ready():   
-  print(f'Logged in as {bot.user}!')
+async def on_ready():
+    DiscordComponents(bot)
+    print(f'Logged in as {bot.user}!')
 
 
 @bot.command()
 async def button(ctx):
-	m= await ctx.send("Here is an example of a button",
-	components=[[
-	Button(style=ButtonStyle.green, label="GREEN"),Button(style=ButtonStyle.red, label="RED"),
-	Button(style=ButtonStyle.grey, label="GREY")],
-	Button(style=ButtonStyle.blue, label="BLUE"),
-	Button(style=ButtonStyle.URL, label="URL", url="https://www.example.com"),
-	],
-  )
+    m = await ctx.send("Here is an example of a button",
+            components=[[
+                Button(style=ButtonStyle.red, label="EMOJI", emoji="😂", disabled=True),
+                Button(style=ButtonStyle.green, label="GREEN"), Button(style=ButtonStyle.red, label="RED"),
+                Button(style=ButtonStyle.grey, label="GREY"), ],
+
+                Button(style=ButtonStyle.blue, label="BLUE"),
+                Button(style=ButtonStyle.URL, label="URL", url="https://www.example.com"),
+             ])
+
 
 @bot.event
 async def on_button_click(m):
+    DiscordComponents(bot)
+    #^ Inits discordcomponents when the event is fired.
+    """Possible interaction types:
+        Pong
+        ChannelMessageWithSource
+        DeferredChannelMessageWithSource
+        DeferredUpdateMessage
+        UpdateMessage
+
+    """
     await m.respond(
+
         type=InteractionType.ChannelMessageWithSource,
         content=f'{m.component.label} pressed'
     )
+
 
 bot.run('TOKEN')
