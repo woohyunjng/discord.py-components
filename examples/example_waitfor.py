4561f361ffba0d8e4468101172f570ed725088e9
PythonSerious <63519852+PythonSerious@users.noreply.github.com>
2021-05-30 12:39:14 -0400

    Move command into example_waitfor.py
    > Put in separate file.

--- examples/example_waitfor.py
+++ examples/example_waitfor.py
@@ -11,22 +11,6 @@ async def on_ready():
     print(f"Logged in as {bot.user}!")
 
 
-@bot.command()
-async def button(ctx):
-    await ctx.send(
-        "Here is an example of a button",
-        components=[
-            [
-                Button(style=ButtonStyle.red, label="EMOJI", emoji="😂"),
-                Button(style=ButtonStyle.green, label="GREEN"),
-                Button(style=ButtonStyle.red, label="RED"),
-                Button(style=ButtonStyle.grey, label="GREY", disabled=True),
-            ],
-            Button(style=ButtonStyle.blue, label="BLUE"),
-            Button(style=ButtonStyle.URL, label="URL", url="https://www.example.com"),
-        ],
-    )
-
 
 @bot.command()
 async def waitforclick(ctx):
@@ -52,22 +36,6 @@ async def waitforclick(ctx):
                 Button(style=ButtonStyle.red, label="Timed out!", disabled=True),
             ]
         )
-
-
-@bot.event
-async def on_button_click(res):
-    """
-    Possible interaction types:
-    - Pong
-    - ChannelMessageWithSource
-    - DeferredChannelMessageWithSource
-    - DeferredUpdateMessage
-    - UpdateMessage
-    """
-
-    await res.respond(
-        type=InteractionType.ChannelMessageWithSource, content=f"{res.component.label} pressed"
-    )
 
 
 bot.run("TOKEN")
