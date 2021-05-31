e15a5b8ec21a0e9b0035bd0c65c55bee3196c667
helish88 <52331802+helish88@users.noreply.github.com>
2021-05-31 14:53:02 +0300

    Update example_wait_for.py
    #52 
    its ok?

--- examples/example_wait_for.py
+++ examples/example_wait_for.py
@@ -36,6 +36,45 @@ async def waitforclick(ctx):
                 Button(style=ButtonStyle.red, label="Timed out!", disabled=True),
             ],
         )
+  
+@bot.command()
+async def buttons(ctx):
+    """Buttons example of navigation with while and with check"""
+    msg = await ctx.channel.send(embed=discord.Embed(color=discord.Color.random(), title="Navigation"),
+                                 components=[
+                                   [Button(style=ButtonStyle.blue, label="Backward", emoji="⏮️"),
+                                    Button(style=ButtonStyle.red, label="Stop", emoji="❌"),
+                                    Button(style=ButtonStyle.blue, label="Forward",  emoji="⏭️")]])
+    def check(m):
+        return m.user == ctx.author and m.message.channel == ctx.channel
+    while True:
+        try:
+            res = await bot.wait_for("button_click", check=check, timeout=60)
+        except asyncio.TimeoutError:
+            break
+            await msg.delete()
+            return await ctx.send(embed=discord.Embed(color=discord.Color.red(),
+                                                      title="TImeout"))
+        if res.channel == ctx.channel:
+            if res.component.label == "Forward":
+                await msg.edit(embed=discord.Embed(color=discord.Color.random(),
+                                                   title="Forward"))
+                await res.respond(
+                    type=InteractionType.DeferredUpdateMessage
+                )
+            elif res.component.label == "Backward":
+                await msg.edit(embed=discord.Embed(color=discord.Color.random(),
+                                                   title="Backward"))
+                await res.respond(
+                    type=InteractionType.DeferredUpdateMessage
+                )
+            elif res.component.label == "Stop":
+                await msg.edit(embed=discord.Embed(color=discord.Color.random(),
+                                                   title="Stop"))
+                await res.respond(
+                    type=InteractionType.UpdateMessage
+                )
+                await msg.delete()
 
 
 bot.run("TOKEN")
