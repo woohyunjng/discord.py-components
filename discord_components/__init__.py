4d099efc274e6815057d814e8572033cae5aff73
kiki7000 <devkiki7000@gmail.com>
2021-05-31 22:12:31 +0900

    chore: upgrade version to 0.5.3

--- discord_components/__init__.py
+++ discord_components/__init__.py
@@ -5,7 +5,7 @@ from .interaction import *
 from .component import *
 
 __name__ = "discord_components"
-__version__ = "0.5.2.4"
+__version__ = "0.5.3"
 
 __author__ = "kiki7000"
 __license__ = "MIT"
