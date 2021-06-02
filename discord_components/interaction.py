f9e16c712e957a08d0b0f91d44563edf1e850677
kiki7000 <devkiki7000@gmail.com>
2021-06-02 19:49:03 +0900

    chore: remove docstring from interaction.py

--- discord_components/interaction.py
+++ discord_components/interaction.py
@@ -1,9 +1,9 @@
-from discord import User, Client, Embed, AllowedMentions, InvalidArgument
+from discord import User, Client, Embed, AllowedMentions, InvalidArgument, Message
 from discord.ext.commands import Bot
 from discord.http import Route
 
 from aiohttp import FormData
-from typing import List
+from typing import List, Union
 from json import dumps
 
 from .button import Button
@@ -18,89 +18,34 @@ InteractionEventType = {"button_click": 2, "select_option": 3}
 
 
 class InteractionType:
-    """Represents interaction types."""
-
-    Pong = 1
-    ChannelMessageWithSource = 4
-    DeferredChannelMessageWithSource = 5
-    DeferredUpdateMessage = 6
-    UpdateMessage = 7
+    Pong: int = 1
+    ChannelMessageWithSource: int = 4
+    DeferredChannelMessageWithSource: int = 5
+    DeferredUpdateMessage: int = 6
+    UpdateMessage: int = 7
 
 
 class FlagsType:
-    """Represents flag types."""
-
-    Crossposted = 1 << 0
-    Is_crosspost = 1 << 1
-    Suppress_embeds = 1 << 2
-    Source_message_deleted = 1 << 3
-    Urgent = 1 << 4
-    Ephemeral = 1 << 6
-    Loading = 1 << 7
+    Crossposted: int = 1 << 0
+    Is_crosspost: int = 1 << 1
+    Suppress_embeds: int = 1 << 2
+    Source_message_deleted: int = 1 << 3
+    Urgent: int = 1 << 4
+    Ephemeral: int = 1 << 6
+    Loading: int = 1 << 7
 
 
 class Interaction:
-    """Contains information about components interact event.
-
-    Parameters
-    ----------
-    bot:  :class:`discord.Client` | :class:`discord.ext.commands.Bot`
-        Discord client to use.
-    client: :class:`~discord_components.DiscordComponents`
-        The client for discord_components.
-    user: :class:`discord.User`
-        The user interacted with the component.
-    component: :class:`~discord_components.Component`
-        The interacted component.
-
-        Component json if ephemeral message
-    raw_data: :class:`dict`
-        JSON sent by discord api.
-    message: :class:`~discord_components.ComponentMessage`
-        The component's message.
-    is_ephemeral: :class:`bool`
-        If ephemeral message
-
-    Attributes
-    ----------
-    bot: :class:`discord.Client` | :class:`discord.ext.commands.Bot`
-        Discord client to use.
-    client: :class:`~discord_components.DiscordComponent`
-        The client for discord_components.
-    user: :class:`discord.User`
-        The user interacted with the component.
-    author: :class:`discord.User`
-        Alias of `user`
-    component: :class:`~discord_components.Component`
-        The interacted component.
-
-        Component json if ephemeral message
-    raw_data: :class:`dict`
-        JSON sent by discord api.
-    message: :class:`discord_components.ComponentMessage`
-        The component's message.
-
-        None if ephemeral message
-    channel: :class:`discord.abc.Messageable`
-        The component message's channel.
-
-        None if ephemeral message
-    guild: :class:`discord.Guild`
-        The component message's guild.
-
-        None if ephemeral message
-    interaction_id: :class:`str`
-        The interaction's ID.
-    interaction_token: :class:`str`
-        The interaction's token.
-    is_ephemeral: :class:`bool`
-        If ephemeral message
-    responded: :class:`bool`
-        If responded?
-    """
-
     def __init__(
-        self, *, bot, client, user=None, component, raw_data, message=None, is_ephemeral=False
+        self,
+        *,
+        bot: Union[Client, Bot],
+        client: "DiscordComponents",
+        user: User = None,
+        component: Component,
+        raw_data: dict,
+        message: Message = None,
+        is_ephemeral: bool = False,
     ):
         self.bot = bot
         self.client = client
@@ -123,44 +68,16 @@ class Interaction:
     async def respond(
         self,
         *,
-        type=InteractionType.ChannelMessageWithSource,
-        content=None,
-        embed=None,
-        embeds=None,
-        allowed_mentions=None,
-        tts=False,
-        ephemeral=True,
-        components=None,
+        type: int = InteractionType.ChannelMessageWithSource,
+        content: str = None,
+        embed: Embed = None,
+        embeds: List[Embed] = None,
+        allowed_mentions: AllowedMentions = None,
+        tts: bool = False,
+        ephemeral: bool = True,
+        components: Union[List[Component], List[List[Component]]] = None,
         **options,
     ) -> None:
-        """Sends response to Discord.
-
-        .. note::
-            If this function is invoked before `wait_for`, a interaction error will be raised.
-
-        :returns: :class:`None`
-
-        Parameters
-        ----------
-        type: :class:`int`
-            The interaction's type. (4 ~ 6)
-            Defaults to ``6``. (InteractionType.ChannelMessageWithSource)
-        content: Optional[:class:`str`]
-            The response message's content.
-        embed: Optional[:class:`discord.Embed`]
-            The response message's embed.
-        embeds: Optional[List[:class:`discord.Embed`]]
-            The response message's embeds.
-        allowed_mentions: Optional[:class:`discord.AllowedMentions`]
-            The response message's allowed mentions.
-        tts: Optional[:class:`bool`]
-            The response message's tts. (Defaults to ``False``)
-        ephemeral: Optional[:class:`bool`]
-            If the response message will be ephemeral (Defaults to ``True``)
-        components: List[:class:`~discord_components.Component` | List[:class:`~discord_components.Component`]]
-            The components to send.
-            If this is 2-dimensional array, an array is a line
-        """
         if self.responded:
             return
 
--- discord_components/message.py
+++ discord_components/message.py
@@ -1,24 +1,13 @@
 from discord import Message
-from typing import List
+from typing import List, Union
 
 from .component import Component
 
 
 class ComponentMessage(Message):
-    """A message with components.
-
-    Parameters
-    ----------
-    components: List[:class:`~discord_components.Component` | List[:class:`~discord_components.Component`]]
-        The message's components.
-
-    Attributes
-    ----------
-    components: List[:class:`~discord_components.Component` | List[:class:`~discord_components.Component`]]
-        The message's components.
-    """
-
-    def __init__(self, *, components=None, **kwargs):
+    def __init__(
+        self, *, components: Union[List[Component], List[List[Component]]] = None, **kwargs
+    ):
         super().__init__(**kwargs)
         if components is None:
             components = []
