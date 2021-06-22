168026c7a5ce23f9d07711973d32d0d63e91df00
devkiki7000 <devkiki7000@gmail.com>
2021-06-22 11:07:44 +0900

    chore: upgrade version to 0.5.4

--- discord_components/__init__.py
+++ discord_components/__init__.py
@@ -4,7 +4,7 @@ from .component import *
 from .message import *
 
 __name__ = "discord_components"
-__version__ = "0.5.4-beta-1"
+__version__ = "0.5.4-beta-2"
 
 __author__ = "kiki7000"
 __license__ = "MIT"
