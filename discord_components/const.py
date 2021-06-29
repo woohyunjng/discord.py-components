c5eb1a09ed011738da846416d4675a89a09f9c3f
devkiki7000 <devkiki7000@gmail.com>
2021-06-29 18:04:05 +0900

    feat(setup): change setup.py to be compatible with discord-py-slash-command (not tested)

---
+++ discord_components/const.py
@@ -0,0 +1,5 @@
+"""Discord Components Constants"""
+
+__version__ = "1.0.0"
+
+BASE_API = "https://discord.com/api/v8"
--- setup.py
+++ setup.py
@@ -1,34 +1,61 @@
-from os import environ
-from setuptools import setup, find_packages
+import re
+from codecs import open
+from os import path, environ
 
-from discord_components import (
-    __author__ as author,
-    __version__ as tempversion,
-    __license__ as _license,
-    __name__ as name,
-)
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "discord_components"
+HERE = path.abspath(path.dirname(__file__))
+
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
 
 try:
-    version = (
+    VERSION = (
         environ["TRAVIS_TAG"].lstrip("v")
         if environ["TRAVIS"] == "true"
         else environ["VERSION_NUMBER"]
     )
 except KeyError:
-    version = tempversion
+    with open(path.join(HERE, PACKAGE_NAME, "const.py"), encoding="utf-8") as fp:
+        VERSION = re.search('__version__ = "([^"]+)"', fp.read()).group(1)
+
+extras = {
+    "lint": ["black", "flake8", "isort"],
+    "readthedocs": ["sphinx", "sphinx-rtd-theme"],
+}
+extras["lint"] += extras["readthedocs"]
+extras["dev"] = extras["lint"] + extras["readthedocs"]
+
 
 setup(
-    name=name,
-    version=version,
-    license=_license,
-    author=author,
+    name="discord_components",
+    version=VERSION,
+    author="kiki7000",
     author_email="devkiki7000@gmail.com",
     description="An unofficial library for discord components.",
-    long_description=open("README.md", encoding="utf-8").read(),
+    extras_require=extras,
+    include_package_data=True,
+    install_requires=["discord.py", "aiohttp"],
+    license="MIT License",
+    long_description=README,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/discord.py-components/discord.py-components",
-    packages=["discord_components", "discord_components.ext"],
-    include_package_data=True,
-    install_requires=open("requirements.txt").readlines(),
-    python_requires=">=3.7",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
 )
