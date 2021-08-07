ca9214a86d7a54e3e5171425776fe0c5e83db1bf
devkiki7000 <devkiki7000@gmail.com>
2021-08-07 09:23:59 +0900

    feat(message): create dpy_overrides file

--- discord_components/dpy_overrides.py
+++ discord_components/dpy_overrides.py
@@ -1,7 +1,12 @@
-from typing import Optional
+from typing import Optional, List, Union
 
-from discord import Message
+from discord import Message, Embed, Attachment, AllowedMentions, InvalidArgument, File
+from discord.http import Route, HTTPClient
+from discord.abc import Messageable, Snowflake
+from discord.utils import to_json
+from discord.ext.commands import Context
 
+from .utils import _get_components_json
 from .component import _get_component_type, ActionRow, Component
 
 __all__ = ("ComponentMessage",)
@@ -18,14 +23,90 @@ class ComponentMessage(Message):
             components.append(ActionRow())
             for j in i["components"]:
                 components[-1].append(_get_component_type(j["type"]).from_json(j))
-        self.components = components
+        self.components: List[ActionRow] = components
 
-    def get_component(self, **checks) -> Optional[Component]:
+    def get_component(self, custom_id: str) -> Optional[Component]:
         for row in self.components:
-            for component in row["components"]:
-                for key, value in checks:
-                    if component.get(key) == value:
-                        return component
+            for component in row.components:
+                if component.custom_id == custom_id:
+                    return component
+
+    async def _components_edit(self, **fields):
+        state = self._state
+        data = {**_get_components_json(fields.get("components"))}
+
+        if fields.get("content") is not None:
+            data["content"] = fields["content"]
+
+        if fields.get("embed") is not None and fields.get("embeds") is not None:
+            raise InvalidArgument(
+                "cannot pass both embed and embeds parameter to edit()"
+            )
+
+        if fields.get("embed") is not None:
+            data["embeds"] = [fields["embed"].to_dict()]
+
+        if fields.get("embeds") is not None:
+            data["embeds"] = [e.to_dict() for e in fields["embeds"]]
+
+        if fields.get("allowed_mentions") is None:
+            if (
+                state.allowed_mentions is not None
+                and self.author.id == self._state.self_id
+            ):
+                data["allowed_mentions"] = state.allowed_mentions.to_dict()
+        else:
+            if state.allowed_mentions is not None:
+                data["allowed_mentions"] = state.allowed_mentions.merge(
+                    fields["allowed_mentions"]
+                ).to_dict()
+            else:
+                data["allowed_mentions"] = fields["allowed_mentions"].to_dict()
+
+        if fields.get("attachments") is not None:
+            data["attachments"] = [a.to_dict() for a in fields["attachments"]]
+
+        if data:
+            await state.http.request(
+                Route("PATCH", f"/channels/{self.channel.id}/messages/{self.id}"),
+                json=data,
+            )
+
+        if fields.get("delete_after") is not None:
+            await self.delete(delay=fields["delete_after"])
+
+    async def edit(
+        self,
+        content: Optional[str] = None,
+        embed: Optional[Embed] = None,
+        embeds: List[Embed] = None,
+        attachments: List[Attachment] = None,
+        delete_after: Optional[float] = None,
+        allowed_mentions: Optional[AllowedMentions] = None,
+        components: List[Union[ActionRow, Component, List[Component]]] = None,
+        **fields,
+    ):
+        if components is not None:
+            await self._components_edit(
+                content=content,
+                embed=embed,
+                embeds=embeds,
+                attachments=attachments,
+                delete_after=delete_after,
+                allowed_mentions=allowed_mentions,
+                components=components,
+            )
+        else:
+            await super().edit(
+                content=content,
+                embed=embed,
+                embeds=embeds,
+                attachments=attachments,
+                delete_after=delete_after,
+                allowed_mentions=allowed_mentions,
+                components=components,
+                **fields,
+            )
 
 
 def new_override(cls, *args, **kwargs):
@@ -36,3 +117,254 @@ def new_override(cls, *args, **kwargs):
 
 
 Message.__new__ = new_override
+
+
+def send_files(
+    self,
+    channel_id: Snowflake,
+    *,
+    files,
+    content=None,
+    tts=False,
+    embed=None,
+    embeds=None,
+    stickers=None,
+    nonce=None,
+    allowed_mentions=None,
+    message_reference=None,
+    components=None,
+):
+    form = []
+
+    payload = {"tts": tts}
+    if content:
+        payload["content"] = content
+    if embed:
+        payload["embeds"] = [embed]
+    if embeds:
+        payload["embeds"] = embeds
+    if nonce:
+        payload["nonce"] = nonce
+    if allowed_mentions:
+        payload["allowed_mentions"] = allowed_mentions
+    if message_reference:
+        payload["message_reference"] = message_reference
+    if stickers:
+        payload["sticker_ids"] = stickers
+    if components:
+        payload["components"] = components
+
+    form.append({"name": "payload_json", "value": to_json(payload)})
+    if len(files) == 1:
+        file = files[0]
+        form.append(
+            {
+                "name": "file",
+                "value": file.fp,
+                "filename": file.filename,
+                "content_type": "application/octet-stream",
+            }
+        )
+    else:
+        for index, file in enumerate(files):
+            form.append(
+                {
+                    "name": f"file{index}",
+                    "value": file.fp,
+                    "filename": file.filename,
+                    "content_type": "application/octet-stream",
+                }
+            )
+
+    return self.request(
+        Route("POST", f"/channels/{channel_id}/messages"), form=form, files=files
+    )
+
+
+def send_message(
+    self,
+    channel_id,
+    content,
+    *,
+    tts=False,
+    embed=None,
+    embeds=None,
+    nonce=None,
+    allowed_mentions=None,
+    message_reference=None,
+    stickers=None,
+    components=None,
+):
+    payload = {}
+
+    if content:
+        payload["content"] = content
+
+    if tts:
+        payload["tts"] = True
+
+    if embed:
+        payload["embeds"] = [embed]
+
+    if embeds:
+        payload["embeds"] = embeds
+
+    if nonce:
+        payload["nonce"] = nonce
+
+    if allowed_mentions:
+        payload["allowed_mentions"] = allowed_mentions
+
+    if message_reference:
+        payload["message_reference"] = message_reference
+
+    if stickers:
+        payload["sticker_ids"] = stickers
+
+    if components:
+        payload["components"] = components
+
+    return self.request(Route("POST", f"/channels/{channel_id}/messages"), json=payload)
+
+
+HTTPClient.send_files = send_files
+HTTPClient.send_message = send_message
+
+
+async def send(
+    self,
+    content=None,
+    *,
+    tts=False,
+    embed=None,
+    embeds=None,
+    file=None,
+    files=None,
+    stickers=None,
+    delete_after=None,
+    nonce=None,
+    allowed_mentions=None,
+    reference=None,
+    mention_author=None,
+    components=None,
+):
+    state = self._state
+    channel = await self._get_channel()
+    content = str(content) if content is not None else None
+
+    if embed is not None and embeds is not None:
+        raise InvalidArgument("cannot pass both embed and embeds parameter to send()")
+
+    if embed is not None:
+        embeds = [embed.to_dict()]
+
+    if embeds is not None:
+        if len(embeds) > 10:
+            raise InvalidArgument(
+                "embeds parameter must be a list of up to 10 elements"
+            )
+        embeds = [embed.to_dict() for embed in embeds]
+
+    if stickers is not None:
+        stickers = [sticker.id for sticker in stickers]
+
+    if allowed_mentions is not None:
+        if state.allowed_mentions is not None:
+            allowed_mentions = state.allowed_mentions.merge(allowed_mentions).to_dict()
+        else:
+            allowed_mentions = allowed_mentions.to_dict()
+    else:
+        allowed_mentions = state.allowed_mentions and state.allowed_mentions.to_dict()
+
+    if mention_author is not None:
+        allowed_mentions = allowed_mentions or AllowedMentions().to_dict()
+        allowed_mentions["replied_user"] = bool(mention_author)
+
+    if reference is not None:
+        try:
+            reference = reference.to_message_reference_dict()
+        except AttributeError:
+            raise InvalidArgument(
+                "reference parameter must be Message or MessageReference"
+            ) from None
+
+    if components is not None:
+        components = _get_components_json(components)
+
+    if file is not None and files is not None:
+        raise InvalidArgument("cannot pass both file and files parameter to send()")
+
+    if file is not None:
+        if not isinstance(file, File):
+            raise InvalidArgument("file parameter must be File")
+
+        try:
+            data = await state.http.send_files(
+                channel.id,
+                files=[file],
+                allowed_mentions=allowed_mentions,
+                content=content,
+                tts=tts,
+                embed=embed,
+                embeds=embeds,
+                nonce=nonce,
+                message_reference=reference,
+                stickers=stickers,
+                components=components,
+            )
+        finally:
+            file.close()
+
+    elif files is not None:
+        if len(files) > 10:
+            raise InvalidArgument("files parameter must be a list of up to 10 elements")
+        elif not all(isinstance(file, File) for file in files):
+            raise InvalidArgument("files parameter must be a list of File")
+
+        try:
+            data = await state.http.send_files(
+                channel.id,
+                files=files,
+                content=content,
+                tts=tts,
+                embed=embed,
+                embeds=embeds,
+                nonce=nonce,
+                allowed_mentions=allowed_mentions,
+                message_reference=reference,
+                stickers=stickers,
+                components=components,
+            )
+        finally:
+            for f in files:
+                f.close()
+    else:
+        data = await state.http.send_message(
+            channel.id,
+            content,
+            tts=tts,
+            embed=embed,
+            embeds=embeds,
+            nonce=nonce,
+            allowed_mentions=allowed_mentions,
+            message_reference=reference,
+            stickers=stickers,
+            components=components,
+        )
+
+    ret = ComponentMessage(state=state, channel=channel, data=data)
+    if delete_after is not None:
+        await ret.delete(delay=delete_after)
+    return ret
+
+
+async def send_override(context_or_channel, *args, **kwargs):
+    if isinstance(context_or_channel, Context):
+        channel = context_or_channel.channel
+    else:
+        channel = context_or_channel
+
+    return await send(channel, *args, **kwargs)
+
+
+Messageable.send = send_override
