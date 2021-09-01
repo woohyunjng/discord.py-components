3b1e0011f02956f734c50b57b2810a8687b9dbc2
Damego <danyabatueff@gmail.com>
2021-09-01 18:02:34 +0500

    Added new event 'on_interaction' and some examples

--- examples/event.py
+++ examples/event.py
@@ -47,4 +47,33 @@ async def on_select_option(interaction):
     await interaction.respond(content=f"{interaction.values[0]} selected!")
 
 
+@bot.command()
+async def interaction(ctx):
+    await ctx.send(
+        "Buttons and Selects!",
+        components=[
+            Select(
+                placeholder="Select something!",
+                options=[
+                    SelectOption(label="a", value="a"),
+                    SelectOption(label="b", value="b"),
+                ],
+                custom_id="select1",
+            ),
+            Button(label="Button", custom_id="button1"),
+        ],
+    )
+
+
+@bot.event
+async def on_interaction(interaction):
+    if interaction.custom_id not in ["select1", "button1"]:
+        return
+
+    if isinstance(interaction.component, Button):
+        await interaction.respond(content="Button clicked!")
+    else:
+        await interaction.respond(content=f"{interaction.values[0]} selected!")
+
+
 bot.run("your token")
